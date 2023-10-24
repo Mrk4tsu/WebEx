d44bceb3a9fe9f8a99d192c15d238c987a0ba016
Nguyễn Đức Thắng <thang.ndu.63cntt@ntu.edu.vn>
2023-10-24 08:04:24 +0700

    Initial commit

---
+++ CNTT_CLC2_NguyenDucThang63135414/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CNTT_CLC2_NguyenDucThang63135414
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ CNTT_CLC2_NguyenDucThang63135414/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CNTT_CLC2_NguyenDucThang63135414.MvcApplication" Language="C#" %>
